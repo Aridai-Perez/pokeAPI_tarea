691235938f949fbbcdb204759677103aa69c392c
Yithzabi Aridai F. Perez <141900042+Aridai-Perez@users.noreply.github.com>
2024-01-17 13:23:43 -0800

    actualización de etiquetas y botones

--- fetch.js
+++ fetch.js
@@ -2,7 +2,9 @@
 //
 // POST
 
-const BASE_URL = 'https://pokeapi.co/api/v2/';
+const BASE_URL = "https://pokeapi.co/api/v2/";
+const IMAGE_CONTAINER = document.querySelector(".img-container");
+const INFO_CONTAINER = document.querySelector(".info-container");
 
 // Fetch no async
 /*
@@ -12,10 +14,10 @@ fetch(BASE_URL + 'pokemon/ditto')
 */
 // fetch async
 
-//GET de la url base, incluyendo el id o nombre de pokemon
 const fetchPokemon = async (pokemon) => {
     try {
         const response = await fetch(`${BASE_URL}pokemon/${pokemon}`);
+        // const response = await fetch(`${BASE_URL} ${pokemon}`);
         const parsedResponse = await response.json();
         return parsedResponse;
     } catch (err) {
@@ -23,88 +25,77 @@ const fetchPokemon = async (pokemon) => {
     }
 }
 
-
-// imprimir el pokemon
+// Obtener pokemon
 document.getElementById('get-btn')
     .addEventListener('click', async () => {
         const text = document.getElementById('poke-name').value.toLowerCase();
         const pokemon = await fetchPokemon(text);
-        localStorage.setItem('currentPokeId', pokemon.name);
+        localStorage.setItem('currentPokeId', pokemon.id);
+        localStorage.setItem('currentPokeName', pokemon.name);
+        localStorage.setItem('currentPokeWeight', pokemon.weight);      
         const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`
-        const name = pokemon.name;
-        const id = pokemon.id;
-        const weight = pokemon.weight;
-        imprimirDoom(id,name,weight,image);
-        console.log(pokemon.name);
+        const name = localStorage.getItem('currentPokeName');
+        const id = localStorage.getItem('currentPokeId');
+        const weight = localStorage.getItem('currentPokeWeight');
+        imprimirDoom(id, name, weight, image);
+        console.log(pokemon.name, pokemon.id, pokemon.weight);
     })
 
 document.addEventListener('DOMContentLoaded', async () => {
     const storedId = localStorage.getItem('currentPokeId');
+    const storedName = localStorage.getItem('currentPokeName');
+    const storedWeight = localStorage.getItem('currentPokeWeight');
     const initialId = storedId ? parseInt(storedId) : 1;
-    const pokemon = await fetchPokemon(initialId);
-    console.log(pokemon.name);
+    const initialName = storedName ? parseInt(storedName) : 1;
+    const initialWeight = storedWeight ? parseInt(storedWeight) : 1;
+    const pokemon = await fetchPokemon(initialId, initialName, initialWeight);
+    console.log(pokemon.name, pokemon.id, pokemon.weight);
 })
 
-// obtener el anterior
-//
-//
-// obtener el siguiente
-
-document.getElementById('previous-btn')
-    .addEventListener('click', async () => {
-        const currentPokeId = parseInt(localStorage.getItem('currentPokeId'));
-        const newId = Math.max(1, currentPokeId -1);
-        const pokemon = await fetchPokemon(newId);
-        console.log(pokemon.name);
-    })
-
-document.getElementById('next-btn')
-    .addEventListener('click', async () => {
-        const currentPokeId = parseInt(localStorage.getItem('currentPokeId'));
-        const newId = currentPokeId + 1;
-        const pokemon = await fetchPokemon(newId);
-        console.log(pokemon);
-    })
 
 
+// // obtener el anterior
+// document.getElementById('previous-btn')
+//     .addEventListener('click', async () => {
+//         const currentPokeId = parseInt(localStorage.getItem('currentPokeId'));
+//         const newId = Math.max(1, currentPokeId - 1);
+//         const pokemon = await fetchPokemon(newId);
+//         console.log(pokemon.name);
+//     })
 
-////////////////// POST
-//
+// // obtener el siguiente
+// document.getElementById('next-btn')
+//     .addEventListener('click', async () => {
+//         const currentPokeId = parseInt(localStorage.getItem('currentPokeId'));
+//         const newId = currentPokeId + 1;
+//         const pokemon = await fetchPokemon(newId);
+//         console.log(pokemon);
+//     })
 
 
-function imprimirDoom (id,name,weight,image) {   
+
+////////////////// IMPRIMIR EN EL HTML
+
+function imprimirDoom(id, name, weight, image) {
     const Pokemon = {
         id: id,
-        user_name: name,
+        name: name,
         weight: weight
     }
     const section = document.getElementById('names')
-    
+
     const pokemon_image = document.createElement('img')
     const user_id = document.createElement('h3');
     const user_name = document.createElement('p')
     const user_weight = document.createElement('p')
     pokemon_image.src = image;
-    user_id.innerHTML = Pokemon.id;
-    user_name.innerHTML = Pokemon.user_name;
-    user_weight.innerHTML = Pokemon.weight;
+    user_id.innerHTML = `ID: ${Pokemon.id}`;
+    user_name.innerHTML = `NAME: ${Pokemon.name}`;
+    user_weight.innerHTML = `WEIGHT: ${Pokemon.weight}`;
 
-    section.append(pokemon_image,user_id,user_name,user_weight)
+    section.append(pokemon_image, user_id, user_name, user_weight)
 
 }
-
-// async function ejemplo(){
-//     const data = await fetch('https://pokeapi.co/api/v2/pokemon/1');
-//     //.then(res => res.json())
-//     //.then(res => console.log(res.json()))
-//     const parsedData = await data.json();
-//     console.log(parsedData.id);
-//     console.log(parsedData.name);
-//     console.log(parsedData.weight);
-//     console.log(parsedData.media)
-// }
-
-// console.log(ejemplo())
 
 /////////////////// EJERCICIOS
 //- Arreglar el pokemon en localStorage
